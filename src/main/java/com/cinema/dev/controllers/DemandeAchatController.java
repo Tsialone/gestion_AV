9a1372a0ef8b8e6a63b62b166fa2623908928b96
Ray <117043408+Snowlydial@users.noreply.github.com>
2026-01-30 13:01:22 +0300

    Affichage/Demande_Achat

--- src/main/java/com/cinema/dev/controllers/DemandeAchatController.java
+++ src/main/java/com/cinema/dev/controllers/DemandeAchatController.java
@@ -3,6 +3,9 @@ package com.cinema.dev.controllers;
 import com.cinema.dev.models.DemandeAchat;
 import com.cinema.dev.models.DemandeAchatDetail;
 import com.cinema.dev.services.DemandeAchatService;
+import com.cinema.dev.repositories.ClientRepository;
+import com.cinema.dev.repositories.FournisseurRepository;
+import com.cinema.dev.repositories.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,10 +18,35 @@ public class DemandeAchatController {
     @Autowired
     private DemandeAchatService demandeAchatService;
     
+    @Autowired
+    private ClientRepository clientRepository;
+    
+    @Autowired
+    private FournisseurRepository fournisseurRepository;
+    
+    @Autowired
+    private ArticleRepository articleRepository;
+    
     @GetMapping("/liste")
     public String getListe(Model model) {
         model.addAttribute("demandesAchat", demandeAchatService.findAll());
         model.addAttribute("content", "pages/demande-achat/demande-achat-liste");
+        return "admin-layout";
+    }
+    
+    @GetMapping("/saisie-client")
+    public String getSaisieClient(Model model) {
+        model.addAttribute("clients", clientRepository.findAll());
+        model.addAttribute("articles", articleRepository.findAll());
+        model.addAttribute("content", "pages/demande-achat/demande-achat-client");
+        return "admin-layout";
+    }
+    
+    @GetMapping("/saisie-fournisseur")
+    public String getSaisieFournisseur(Model model) {
+        model.addAttribute("fournisseurs", fournisseurRepository.findAll());
+        model.addAttribute("articles", articleRepository.findAll());
+        model.addAttribute("content", "pages/demande-achat/demande-achat-fournisseur");
         return "admin-layout";
     }
     
