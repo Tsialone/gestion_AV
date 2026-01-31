0267e739c33e833e19434cef3e8162d290d32854
Ray <117043408+Snowlydial@users.noreply.github.com>
2026-01-31 09:51:51 +0300

    Update ArticleController.java

--- src/main/java/com/cinema/dev/controllers/ArticleController.java
+++ src/main/java/com/cinema/dev/controllers/ArticleController.java
@@ -1,10 +1,13 @@
 package com.cinema.dev.controllers;
 
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cinema.dev.utils.BreadcrumbItem;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,6 +22,9 @@ public class ArticleController {
     public String getListe(Model model) {
         // model.addAttribute("articles", articleService.findAll());
         model.addAttribute("content", "pages/articles/article-liste");
+        model.addAttribute("breadcrumbs", Arrays.asList(
+            new BreadcrumbItem("Articles", "/articles/liste")
+        ));
         return "admin-layout";
     }
 }
